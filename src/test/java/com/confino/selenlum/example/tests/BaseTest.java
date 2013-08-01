d43b54c6d722368a3f9372bb14df0b0275a54772
unknown <aconfino@gmail.com>
2013-08-01 07:32:31 -0400

    updated

--- src/test/java/com/confino/selenlum/example/tests/BaseTest.java
+++ src/test/java/com/confino/selenlum/example/tests/BaseTest.java
@@ -1,11 +1,8 @@
 package com.confino.selenlum.example.tests;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.confino.selenium.example.utils.DriverUtils;
 
